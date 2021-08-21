f55713df0df1b52eb0bb3bee77d4b38a85b930e0
Aminat-Okunuga <makadeaminat@gmail.com>
2021-08-21 11:01:06 +0100

    modified index.php

--- index.php
+++ index.php
@@ -1,19 +1,52 @@
 <?php
+include "db.php";
+
 $msg = "";
 if(isset($_POST['submit'])){
-    if(empty($_POST['username']) || empty($_POST['firstname']) || empty($_POST['lastname']) || empty($_POST['email'])){
+    if(empty($_POST['username']) && empty($_POST['firstname']) && empty($_POST['lastname']) && empty($_POST['email'])){
         $msg = "Fields cannot be empty!";
-        // header("Location: index.php");
     }elseif ((empty($_POST['username']) || ($_POST['username'] == ""))) {
-        echo "Username is required!";
-    }elseif ((empty($_POST['firstname']) || ($_POST['firstname'] == ""))) {
-        echo "First NAme is required!";
-    }elseif ((empty($_POST['lastname']) || ($_POST['lastname'] == ""))) {
-        echo "Last Name is required!";
+        $msg = "Username is required!";
+    }elseif ((empty($_POST['first_name']) || ($_POST['first_name'] == ""))) {
+        $msg = "First Name is required!";
+    }elseif ((empty($_POST['last_name']) || ($_POST['last_name'] == ""))) {
+        $msg = "Last Name is required!";
     }elseif ((empty($_POST['email']) || ($_POST['email'] == ""))) {
-        echo "Email is required!";
+        $msg = "Email is required!";
+    } else{
+        $username = mysqli_real_escape_string($connect, $_POST['username']);
+        $first_name = mysqli_real_escape_string($connect, $_POST['first_name']);
+        $last_name = mysqli_real_escape_string($connect, $_POST['last_name']);
+        $email = mysqli_real_escape_string($connect, $_POST['email']);
+
+        // insert into database
+        $create_user = mysqli_query($connect, "INSERT INTO users (first_name, last_name, email) VALUES ('$first_name', '$last_name', '$email') ");
+        if($create_user){
+            $msg = "User Record Has Been Created Successfully!";
+        }else{
+            $msg = "Something Went Wrong!";
+        }
     }
 }
+
+// fetch all users record
+$all_user = mysqli_query($connect, "SELECT * FROM users");
+
+// delete user record
+if(isset($_GET['delete'])){
+    $id = $_GET['delete'];
+    $delete_user = mysqli_query($connect, "DELETE FROM users WHERE id = $id");
+    if($delete_user){
+        $msg = "User Record Has Been Deleted Successfully!";
+        header("location: index.php");
+    }else{
+        $msg = "Something Went Wrong!";
+    }
+}
+
+// edit user record
+
+
 
 ?>
 
@@ -42,9 +75,43 @@ if(isset($_POST['submit'])){
             <label for="Last Name">Last Name:</label>
             <input class="form-input" type="text" name="last_name" id=""><br>
             <label for="Email">Email:</label>
-            <input id="email" class="form-input" type="text" name="email" id=""><br>
+            <input id="email" class="form-input" type="email" name="email" id=""><br>
             <input class="form-input" type="submit" name="submit" value="Submit">
         </form>
+    </div>
+
+    <!-- all users table -->
+    <h2>All Users</h2>
+    <div class="table-container">
+        <table cellspacing = "40px" class="table">
+            <thead>
+                <tr>
+                    <!-- <th>S/N</th> -->
+                    <th>Username</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+            
+                foreach($all_user as $user){?>
+                <tr>
+                    <td><?php echo $user['first_name']; ?></td>
+                    <td><?php echo $user['last_name']; ?></td>
+                    <td><?php echo $user['email']; ?></td>
+                    <td>
+                        <a class="edit" href="edit.php?edit=<?php echo $user['id']; ?>">Edit</a>
+                        <a class="delete" href="index.php?delete=<?php echo $user['id']; ?>">Delete</a>
+                    </td>
+                </tr>
+                <?php }
+                    // }
+                // } 
+                ?>
+            </tbody>
+        </table>
     </div>
 </body>
 </html>
